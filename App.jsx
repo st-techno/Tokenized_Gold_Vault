13b5e668a9973e93d4ae362d10e9a7a6fafe95b1
ST Technologies <98237904+st-techno@users.noreply.github.com>
2025-10-04 06:55:17 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+
+import React from 'react';
+import VaultDashboard from './components/VaultDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <VaultDashboard />
+    </div>
+  );
+}
+
+export default App;
